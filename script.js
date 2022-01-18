4f57fe2d05f080171183be890f06b29f55745248
Jasmine Scott <jas_scott2011@yahoo.com>
2022-01-18 18:20:53 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-function compute()
+function computeInterest()
 {
-    p = document.getElementById("principal").value;
-    
+     var principal = document.getElementById("principal").value;
+
+     //Check if principal  is 0 or negative
+    if(principal < 1){
+        alert("Enter a positive number.");
+        document.getElementById("principal").focus();
+        return false;
+    }
+     var rate = document.getElementById("rate").value;
+     var years = document.getElementById("years").value;
+     //Calculate Interest
+     var interest = principal*years*(rate/100);
+     var year = new Date().getFullYear()+parseInt(years) ;
+     var amount= principal+interest;
+     //show the result
+     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval +"%";
+    slider.onchange = function() {
+        output.innerHTML = this.value;}
+}
